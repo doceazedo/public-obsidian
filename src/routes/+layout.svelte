cb51f943ad30fb6e1f6caaaa061959f806b19225
doceazedo <me@doceazedo.com>
2023-05-30 16:54:47 -0300

    Translate footer text

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,11 +7,11 @@
 
 <footer class="container">
 	<small>
-		Built with
+		Feito com
 		<a href="https://kit.svelte.dev" target="_blank" rel="noopener noreferrer">SvelteKit</a>,
-		<a href="https://picocss.com" target="_blank" rel="noopener noreferrer">Pico</a> and
+		<a href="https://picocss.com" target="_blank" rel="noopener noreferrer">Pico</a> e
 		<a href="https://mega.js.org/" target="_blank" rel="noopener noreferrer">MEGA</a>
 		&bull;
-		<a href="https://github.com/doceazedo/public-todos">Source code</a>
+		<a href="https://github.com/doceazedo/public-todos">Código-fonte</a>
 	</small>
 </footer>
